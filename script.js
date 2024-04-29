7ad5bf391cea585f2e533a60530987149cdee59f
Edgar Alcina <160753417+Edzin32@users.noreply.github.com>
2024-04-29 11:02:31 +0000

    alteração option4

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-
 var canvas = document.getElementById('Canvas');
 var ctx = canvas.getContext('2d');
 
 var img = new Image();
-var x = 0;
-var y = 0;
-img.src = 'img/nave.png'; 
-
-
- 
+var x = 530;
+var y = 250;
+img.src = 'img/nave.png';
     function update() {
+        
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(img, x, y, 80, 80); 
-        x += 1;
-        y+=1;
-        if (x < canvas.width - 79) {
+        x=x-1;
+        
+        if (x > canvas.width-600) {
             requestAnimationFrame(update);
         }
     }
 
     update();
-
